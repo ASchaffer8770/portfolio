d44a8afee33b94e88e5fa6a41b65d269a94625aa
ASchaffer8770 <aschaffer8770@gmail.com>
2023-03-07 20:32:28 -0800

    Projects Component

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
         <div className='w-full h-screen absolute top-0 left-0 bg-white/30'>
             <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
                 <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Schaffer.Tech</h1>
-                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I create 
+                <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>I Create 
                     <TypeAnimation
                         sequence={[
                         'Full Stack Web Applications', // Types 'One'
