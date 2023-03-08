d4094333d8838806259a85358d932884de0d0807
Alex <alex@lite-check.com>
2023-03-08 14:31:18 -0800

    project tiles and smooth scroll

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -4,7 +4,17 @@ import propertyImg from '../assets/pictures/WIP.png'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+      <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>  
+      <p className='text-center py-8'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, eius aut quasi, sequi optio mollitia minima delectus saepe accusamus minus inventore eaque quia iste id odio tenetur aspernatur, hic sunt!
+      </p>
+      <div className='grid sm:grid-cols-2 gap-12'>
+        <ProjectItem img={propertyImg} title='Work in Progress'/>
+        <ProjectItem img={propertyImg} title='Work in Progress'/>
+        <ProjectItem img={propertyImg} title='Work in Progress'/>
+        <ProjectItem img={propertyImg} title='Work in Progress'/>
+      </div>
+    </div>
   )
 }
 
