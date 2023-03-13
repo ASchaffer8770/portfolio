a883c9e80107b6f1f0af49c6aa1a3871a8d59196
Alex <alex@lite-check.com>
2023-03-13 14:23:24 -0700

    react router install

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -6,7 +6,8 @@ const Projects = () => {
   return (
     <div id='projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>  
-      <p className='text-center py-8'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos, eius aut quasi, sequi optio mollitia minima delectus saepe accusamus minus inventore eaque quia iste id odio tenetur aspernatur, hic sunt!
+      <p className='text-center py-8'>
+        Current projects are in progress and will be released based on user testing and feedback. Thanks for your patience.
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
         <ProjectItem img={propertyImg} title='Work in Progress'/>
